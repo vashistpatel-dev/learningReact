88b8448c6fc46588d5a00cc4f5518666d8f08fab
vashistpatel-dev <vashist21patel@gmail.com>
2025-01-15 19:48:42 -0500

    Added Components, Porps and new data.js file

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,16 +1,50 @@
+import reactImg from './assets/react-core-concepts.png';
+import componentsImg from './assets/components.png';
+
+const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
+function Header() {
+  const description = reactDescriptions[genRandomInt(2)];
+
+  return (
+    <header>
+      <img src={reactImg} alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        {description} React concepts you will need for almost any app you are
+        going to build!
+      </p>
+    </header>
+  );
+}
+
+function CoreConcepts(props) {
+  return (
+    <li>
+      <img src={props.img} alt={props.title} />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
+  );
+}
+
 function App() {
   return (
     <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
+      <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id='core-concepts'>
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcepts title='Componenets' description='The core UI building block.' img={componentsImg} />
+            <CoreConcepts title='Componenets' description='The core UI building block.' img={componentsImg} />
+            <CoreConcepts title='Componenets' description='The core UI building block.' img={componentsImg} />
+            <CoreConcepts title='Componenets' description='The core UI building block.' img={componentsImg} />
+          </ul>
+        </section>
       </main>
     </div>
   );
