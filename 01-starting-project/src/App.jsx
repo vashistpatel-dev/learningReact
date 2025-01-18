25d9374474fd1796e74d906787764a853546585d
vashistpatel-dev <vashist21patel@gmail.com>
2025-01-18 14:04:43 -0500

    Added Fragment tag or empy tag to avoid extra div in DOM

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, Fragment } from 'react';
 import { CORE_CONCEPTS } from './data.js';
 import Header from './components/Header/Header.jsx';
 import CoreConcepts from './components/CoreConcepts.jsx';
@@ -34,7 +34,7 @@ function App() { // App component function is only encountred in the beining hen
   }
 
   return (
-    <div>
+    <> {/* we can use fragment if you dont want to render a extra div in the DOM, or keep it empty */}
       <Header />
       <main>
         <section id='core-concepts'>
@@ -75,7 +75,7 @@ function App() { // App component function is only encountred in the beining hen
         </section>
 
       </main>
-    </div>
+    </>
   );
 }
 
