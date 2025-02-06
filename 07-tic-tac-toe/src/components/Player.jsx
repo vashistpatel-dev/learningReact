47e1613a8dda4a5f053036b131b0bea87f1498e0
vashistpatel-dev <vashist21patel@gmail.com>
2025-02-05 20:19:13 -0500

    Now able to edit player names and save it

--- 07-tic-tac-toe/src/components/Player.jsx
+++ 07-tic-tac-toe/src/components/Player.jsx
@@ -1,21 +1,29 @@
 import { useState } from "react";
 
-export default function Player({ name, symbol }) {
+export default function Player({ initialName, symbol }) {
+    const [playerName, setPlayerName] = useState(initialName);
     const [isEditing, setIsEditing] = useState(false);
 
     function handleEditClick() { // start fucntion name with handle when it is triggered upon an event
-        setIsEditing(isEditing ? false : true); // can do it like this: !isEditing
+        // setIsEditing(isEditing ? false : true); // can do it like this: !isEditing, but shouldnt due to asynchronous updating of states
+        // setIsEditing((editing) => !editing);
+        setIsEditing(editing => !editing);
     }
 
-    let playerName = <span className="player-name">{name}</span>;
+    function handleChange(event) { //we want to listen to the change event here
+        // console.log(event)
+        setPlayerName(event.target.value);
+    }
+
+    let editablePlayerName = <span className="player-name">{playerName}</span>;
     if (isEditing) {
-        playerName = <input type="text" required value={name} />;
+        editablePlayerName = <input type="text" required value={playerName} onChange={handleChange} />;
     }
 
     return (
         <li>
             <span className="player">
-                {playerName}
+                {editablePlayerName}
                 <span className="player-symbol">{symbol}</span>
             </span>
             <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button> {/* passing fucntion as a value */}
